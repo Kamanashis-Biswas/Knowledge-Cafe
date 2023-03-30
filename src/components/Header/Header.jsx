c390724ba2d8c21596cc27f22567d09112103f50
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2023-03-31 04:00:51 +0600

    Header Complete

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <h2>Nestle Cafe</h2>
+      <img
+        src="https://images.unsplash.com/photo-1566753323558-f4e0952af115?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80"
+        alt=""
+      />
+    </nav>
+  );
+};
+
+export default Header;
